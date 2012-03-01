7ed7cfea1e3be88d6f808009cb0d5af1b93909df
Raffi Hovagimian <rhovagimian@modelmetrics.com>
2012-03-01 18:54:33 -0500

    Added code to save to salesforce

--- src/main/java/com/modelmetrics/CreateOrderServlet.java
+++ src/main/java/com/modelmetrics/CreateOrderServlet.java
@@ -1,13 +1,23 @@
 package com.modelmetrics;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Enumeration;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.parser.JSONParser;
 
 public class CreateOrderServlet extends HttpServlet {
 
@@ -22,47 +32,58 @@ public class CreateOrderServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
-		List<String> attributeNames = Collections.list((Enumeration<String>)req.getSession().getAttributeNames());
-		for (String attributeName : attributeNames){
-			System.out.println(attributeName + " = " + req.getSession().getAttribute(attributeName));
+		try {
+			Configuration config = (Configuration)req.getSession().getAttribute("configuration");
+			if(config == null || config.getAccessToken() == null || config.getAccessToken().length() <= 10) {
+				resp.sendRedirect("index.jsp");
+			} else { 
+				String interior = req.getParameter("interior");
+				double totalPrice = Double.parseDouble(req.getParameter("totalPrice"));
+				String imageLink = req.getParameter("vehImg");
+				String options = req.getParameter("options");
+				String signature = req.getParameter("signature");
+				
+				String orderData = " {  \"Interior__c\" : \"" + interior + "\", \"Total_Price__c\" : " 
+					+ totalPrice + ", \"ImageLink__c\" : \"" + imageLink +"\", \"Options__c\" : \"" + options + "\" } ";
+				String url = config.getEndpoint() + "sobjects/Sales_Record__c";
+				//post data to salesforce
+	            String orderId = postSalesforceData(url, orderData, config.getAccessToken());
+	            
+	            //post signature to salesforce
+	            String signatureData = " {  \"Body\" : \"" + signature + "\", \"ParentId\" : \"" + orderId + "\", \"Name\" : \"Signature.png\", \"ContentType\" : \"image/png\" } ";
+	            url = config.getEndpoint() + "sobjects/Attachment";
+	            String response = postSalesforceData(url, signatureData, config.getAccessToken());
+	            resp.getWriter().write(response);
+			}
 		}
-		
-		List<String> parameterNames = Collections.list((Enumeration<String>)req.getParameterNames());
-		for (String parameterName : parameterNames){
-			System.out.println(parameterName + " = " + req.getParameter(parameterName));
+		catch(Exception ex) {
+			ex.printStackTrace(System.err);
+			resp.sendError(404, ex.getMessage());
 		}
+	}
 
-		/*if (!String.IsNullOrEmpty(ConnectionSettings.AccessToken) && ConnectionSettings.AccessToken.Length > 10)
-        {
-            WebRequest request = createRequest(ConfigurationSettings.AppSettings["endpoint"] + "sobjects/Sales_Record__c", "POST");
-
-            string JSONText = " {  \"Interior__c\" : \"" + interiorSelected + "\", \"Total_Price__c\" : " + Decimal.Parse(price) + ", \"ImageLink__c\" : \"" + imageLink +"\", \"Options__c\" : \"" + opts + "\" } ";
-            //string JSONText = " {  \"Interior__c\" : \"" + interiorSelected + "\", \"Total_Price__c\" : \"" + Decimal.Parse(price) + "\", \"Options__c\" : \"" + opts + "\" } ";
-
-            //string JSONText = " {  \"Interior__c\" : \"" + interiorSelected + "\" } ";
-            
-
-
-
-            WebResponse response = attachData(request, JSONText).GetResponse();
-
-            resp = readResponse(response);
-            //resp = JSONText;
-        }
-		string resp = "";
-
-        if (!String.IsNullOrEmpty(ConnectionSettings.AccessToken) && ConnectionSettings.AccessToken.Length > 10)
-        {
-            WebRequest request = createRequest(ConfigurationSettings.AppSettings["endpoint"] + "sobjects/Attachment", "POST");//WebRequest.Create("https://prerelna1.pre.salesforce.com/services/data/v20.0/sobjects/Attachment");
-
-            string JSONText = " {  \"Body\" : \"" + sigBlob + "\", \"ParentId\" : \"" + orderID + "\", \"Name\" : \"Signature.png\", \"ContentType\" : \"image/png\" } ";
-
-            WebResponse response = attachData(request, JSONText).GetResponse();
-
-            resp = readResponse(response);
+	
+	private String postSalesforceData(String url, String data, String accessToken) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+        connection.setRequestMethod("POST");
+        connection.addRequestProperty("Authorization","OAuth " + accessToken);
+        connection.addRequestProperty("X-PrettyPrint", "1");
+        connection.addRequestProperty("Content-Type", "application/json");
+        connection.connect();
+        OutputStream output = connection.getOutputStream();
+        String encodeData = URLEncoder.encode(data, "UTF-8");
+        output.write(encodeData.getBytes("UTF-8"));
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        String response = "";
+        while ((inputLine = in.readLine()) != null) {
+        	response += inputLine;
         }
 
-        return resp;*/
+        in.close();
+        connection.disconnect();
+        
+        return response;
 	}
 }
